d36299fa576be177c2df35b3fd9893198579be60
Don Sirivat <don@Dons-MacBook-Pro.local>
2024-05-16 12:33:52 -0700

    Add changeOrigin option

--- server.js
+++ server.js
@@ -39,10 +39,10 @@ cors_proxy.createServer({
     // 'x-forwarded-proto',
     // 'x-forwarded-port',
   ],
-  redirectSameOrigin: true,
   httpProxyOptions: {
     // Do not add X-Forwarded-For, etc. headers, because Heroku already adds it.
     xfwd: false,
+    changeOrigin: true,
   },
 }).listen(port, host, function() {
   console.log('Running CORS Anywhere on ' + host + ':' + port);
